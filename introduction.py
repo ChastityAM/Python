1c0cd37acf1ebc2926b4fc0c381b99361091d37e
ChastityAM <chastity.mascarenas.e@gmail.com>
2021-02-23 17:11:16 -0500

    Added Math functions

---
+++ introduction.py
@@ -0,0 +1,13 @@
+print("Hello World")
+name='John'
+print(name)
+print(1+6)
+print(5*5)
+print(7/2, 7//2) # div float div int
+print(2**4)
+print(28%3)#module
+var1=6
+var2=8
+print(var1 + var2)
+print(var2 - var1)
+print(var1 % var2)
