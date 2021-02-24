86fee5ee00fabd34d458f9a93389fd11c19ad57b
ChastityAM <chastity.mascarenas.e@gmail.com>
2021-02-24 12:04:15 -0500

    added string functions

--- functions.py
+++ functions.py
@@ -3,10 +3,10 @@ def weird_arithmetic(x, y, z): #function defined
 
 weird_arithmetic(5, 6, 7) #function called
 
-def greeting(a, b):
-    print(a + b + "!")
+def greet_friend(greeting, name):
+    print(greeting, name + "!")
 
-greeting("Hello ", "Bethany")
+greet_friend("Hello", "Bethany")
 
 def my_sum(n1, n2):
     return n1 + n2
@@ -16,14 +16,25 @@ some_number=my_sum(8, 3)
 print(some_number)
 
 def area(a, b):
-    return a * (b**2)
+    return a * b ** 2
 c_area=area(3.14, 4)
 
 print(c_area)
 
 def volume(c_area, height):
-    return(c_area * height)
+    print(c_area * height)
 
-cylinder=volume(c_area, 10)
+volume(c_area, 10)
 
-print(cylinder)
+def friend_greets(name, greeting, sentence):
+    output = "{0},{1}! {2}".format(greeting, name, sentence)
+    print(output)
+
+friend_greets("John", "Hey", "How you doin?")
+
+def greet_user(greeting):
+    user = input("Please enter your name.\n")
+    print("{},{}!".format(greeting, user))
+
+    print(5 + int(input("Please enter an integer.")))
+greet_user("Hello")
