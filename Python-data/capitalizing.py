c45dee7b23e8dfd4c8d163723cb7858e0d09017b
Chastity M <74201085+ChastityAM@users.noreply.github.com>
2021-04-26 17:03:47 -0400

    Uploading of my small projects from this month

---
+++ Python-data/capitalizing.py
@@ -0,0 +1,5 @@
+full_name = input("Enter first and last name: ").split(" ")
+first_name = full_name[0].lower().capitalize()
+last_name = full_name[1].lower().capitalize()
+print(f"first_name: {first_name}, last_name: {last_name}")
+
